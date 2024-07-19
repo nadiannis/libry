46c5ae6b1d862640c70ddf5f516e536d82ec8a23
nadiannis <annisanadianeyla@gmail.com>
2024-07-19 17:48:49 +0700

    chore: add TimeIsBetween util function

---
+++ internal/utils/validation.go
@@ -0,0 +1,10 @@
+package utils
+
+import "time"
+
+func TimeIsBetween(t, min, max time.Time) bool {
+	if min.After(max) {
+		min, max = max, min
+	}
+	return (t.Equal(min) || t.After(min)) && (t.Equal(max) || t.Before(max))
+}
